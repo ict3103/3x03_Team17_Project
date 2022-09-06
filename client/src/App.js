2d86a18ca6167d13acb144d175c61a1e07292831
yujing1314276 <yujing1314276@gmail.com>
2022-09-06 21:17:14 +0800

    Revert "Revert "initial commit""
    This reverts commit 5bbefbab40e11d147f8380c573b7fbb821a6b2ec.

---
+++ client/src/App.js
@@ -0,0 +1,59 @@
+import './App.css';
+import {React} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Route} from 'react-router-dom';
+import { CartProvider} from "react-use-cart";
+import './components/boilerplates/Main';
+import Main from './components/boilerplates/Main';
+import Login from './components/boilerplates/Login';
+import Register from './components/boilerplates/Register';
+import Collection from './components/boilerplates/Collection';
+import AdminDashboard from './components/boilerplates/AdminDashboard';
+import AddItem from './components/boilerplates/AddItem';
+import EditItem from './components/boilerplates/EditItem';
+import CollectionLogin from './components/boilerplates/CollectionLogin';
+import ShoppingCart from './components/boilerplates/ShoppingCart';
+import { Page,Cart } from './components/boilerplates/ShoppingCart';
+
+
+
+function App() {
+  return (
+    <div>
+       <Route path="/addItem">
+        <AddItem >
+        </AddItem>  
+      </Route>
+      <Route path="/editItem">
+        <EditItem >
+        </EditItem>  
+      </Route>
+      <Route path="/adminDashboard">
+        <AdminDashboard >
+        </AdminDashboard>  
+      </Route>
+      <Route path="/cart">
+        <ShoppingCart></ShoppingCart>
+      </Route>
+       <Route path="/collection">
+        <Collection ></Collection>   
+      </Route>
+      <Route path="/collectionLogin">
+        <CollectionLogin ></CollectionLogin>   
+      </Route>
+      <Route exact path="/">
+        <Main></Main>
+      </Route>
+      <Route path="/login">
+      <Main></Main>
+        <Login></Login>
+      </Route>
+      <Route path="/register">
+      <Main></Main>
+        <Register></Register>
+      </Route>
+    </div>
+  );
+}
+
+export default App;
