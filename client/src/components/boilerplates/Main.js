4a7f013de2eb66cec8d127fbc4f0b5329f3b6649
yujing1314276 <yujing1314276@gmail.com>
2022-09-17 00:37:48 +0800

    resolve warnings

--- client/src/components/boilerplates/Main.js
+++ client/src/components/boilerplates/Main.js
@@ -8,33 +8,33 @@ function Main() {
   return (
 
     <div >
-      <video autoPlay loop muted class="back-video">
+      <video autoPlay loop muted className="back-video">
       <source src={require("../../images/laptop_video.mp4")} type="video/mp4"/>
     </video>
   
-    <nav class="navbar navbar-expand-lg navbar-dark">
-    <div class="container-fluid">    
+    <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: "#060707"}}>
+    <div className="container-fluid">    
       <FaReact className="App-logo" />
 
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <Link class="nav-link" aria-current="page" to="/">Home</Link>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <Link className="nav-link" aria-current="page" to="/">Home</Link>
           </li>
-          <li class="nav-item">
-            <Link class="nav-link" to="/collection">Shop</Link>
+          <li className="nav-item">
+            <Link className="nav-link" to="/collection">Shop</Link>
           </li>
         </ul>
-        <form class="d-flex">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <Link class="nav-link" aria-current="page" to="register">Register</Link>
+        <form className="d-flex">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link className="nav-link" aria-current="page" to="register">Register</Link>
                 </li>
-                <li class="nav-item">
-                    <Link class="btn btn-dark" to="login" style={{"border-radius": "30px;","background-color": "transparent"}}role="button">Login</Link>
+                <li className="nav-item">
+                    <Link className="btn btn-dark" to="login" style={{borderRadius: "30px;","backgroundColor": "transparent"}}role="button">Login</Link>
                 </li>       
               </ul>
         </form>
