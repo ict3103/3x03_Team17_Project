c24884bdc5aa9c355848b80fc083c6fdee0c248b
yujing1314276 <yujing1314276@gmail.com>
2022-09-17 11:03:01 +0800

    update react file structure

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,14 @@ import {Route} from 'react-router-dom';
 import './components/boilerplates/Main';
 import Main from './components/boilerplates/Main';
 import Login from './components/boilerplates/Login';
-import Register from './components/boilerplates/Register';
+import Register from './components/boilerplates/register/Register';
 import Collection from './components/boilerplates/Collection';
-import AdminDashboard from './components/boilerplates/AdminDashboard';
-import AddItem from './components/boilerplates/AddItem';
-import EditItem from './components/boilerplates/EditItem';
-import CollectionLogin from './components/boilerplates/CollectionLogin';
-import ShoppingCart from './components/boilerplates/ShoppingCart';
-
-import VerificationPage from './components/boilerplates/VerificationPage';
-import VerifiedPage from './components/boilerplates/VerifiedPage';
-import ResetPasswordPage from './components/boilerplates/ResetPasswordPage';
-import ResetSuccess from './components/boilerplates/ResetSuccessPage';
-import Payment from './components/boilerplates/Payment'
+import ShoppingCart from './components/boilerplates/cart/ShoppingCart';
+import VerificationPage from './components/boilerplates/register/VerificationPage';
+import VerifiedPage from './components/boilerplates/register/VerifiedPage';
+import ResetPasswordPage from './components/boilerplates/resetPassword/ResetPasswordPage';
+import ResetSuccess from './components/boilerplates/resetPassword/ResetSuccessPage';
+import Payment from './components/boilerplates/cart/Payment'
 
 
 
@@ -25,7 +20,43 @@ function App() {
 
   return (
     <div>
-       <Route path="/addItem">
+      <Route exact path="/">
+        <Main></Main>
+      </Route>
+      <Route path="/collection">
+        <Collection ></Collection>   
+      </Route>
+      <Route path="/register">
+        <Main></Main>
+        <Register></Register>
+      </Route>
+      <Route path="/login">
+        <Main></Main>
+        <Login></Login>
+      </Route>
+      <Route path="/cart">
+        <ShoppingCart></ShoppingCart>
+      </Route>
+      <Route path="/payment">
+        <Payment></Payment>
+      </Route>
+      <Route path="/verification">
+        <Main></Main>
+        <VerificationPage></VerificationPage>
+      </Route>
+      <Route path="/verifiedPage">
+        <VerifiedPage></VerifiedPage>
+      </Route>
+      <Route path="/resetPassword/:token">
+        <Main></Main>
+        <ResetPasswordPage ></ResetPasswordPage>
+      </Route>
+      <Route path="/resetPasswordSuccess">
+        <Main></Main>
+        <ResetSuccess ></ResetSuccess>
+      </Route>
+
+       {/* <Route path="/addItem">
         <AddItem >
         </AddItem>  
       </Route>
@@ -36,45 +67,7 @@ function App() {
       <Route path="/adminDashboard">
         <AdminDashboard >
         </AdminDashboard>  
-      </Route>
-      <Route path="/cart">
-        <ShoppingCart></ShoppingCart>
-      </Route>
-       <Route path="/collection">
-        <Collection ></Collection>   
-      </Route>
-      <Route path="/collectionLogin">
-        <CollectionLogin ></CollectionLogin>   
-      </Route>
-      <Route path="/verification">
-        <Main></Main>
-        <VerificationPage></VerificationPage>
-      </Route>
-      <Route path="/verifiedPage">
-        <VerifiedPage></VerifiedPage>
-      </Route>
-      <Route exact path="/">
-        <Main></Main>
-      </Route>
-      <Route path="/login">
-        <Main></Main>
-        <Login></Login>
-      </Route>
-      <Route path="/register">
-        <Main></Main>
-        <Register></Register>
-      </Route>
-      <Route path="/resetPassword/:token">
-        <Main></Main>
-        <ResetPasswordPage ></ResetPasswordPage>
-      </Route>
-      <Route path="/resetPasswordSuccess">
-        <Main></Main>
-        <ResetSuccess ></ResetSuccess>
-      </Route>
-      <Route path="/payment">
-        <Payment></Payment>
-      </Route>
+      </Route> */}
     </div>
   );
 }
