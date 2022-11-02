f5a7228bf43d09414f838a6c80ed2b9129c22987
yujing1314276 <yujing1314276@gmail.com>
2022-11-03 00:53:24 +0800

    Update app.py

--- server/app.py
+++ server/app.py
@@ -20,13 +20,6 @@ import geocoder, time
 
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
 
-# for cookies and fresh cookies
-# from flask_jwt_extended import get_jwt
-# from flask_jwt_extended import set_access_cookies
-# from flask_jwt_extended import set_refresh_cookies
-# from flask_jwt_extended import unset_jwt_cookies
-# from flask_jwt_extended import create_refresh_token
-
 
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = '159.223.91.38'
@@ -39,36 +32,11 @@ mysql = MySQL(app)
 cors = CORS(app)
 mail = Mail(app)
 
-# If cookie_secure = true,only allow JWTs to be sent over https. 
-# In production, this should always be set to True
-app.config["JWT_COOKIE_SECURE"] = False
-app.config["JWT_TOKEN_LOCATION"] = ["headers", "cookies", "json", "query_string"]
-#jwt expiry
+app.config["JWT_TOKEN_LOCATION"] = ["headers"]
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=2)
-# app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
 app.config['JWT_SECRET_KEY'] = 'super-secret' #as of now
-
-# Disable CSRF protection for this example. In almost every case,
-# this is a bad idea. See examples/csrf_protection_with_cookies.py
-# for how safely store JWTs in cookies
-app.config['JWT_COOKIE_CSRF_PROTECT'] = False
 #create jwt manager
 jwt = JWTManager(app)
-
-#fresh JWT token in case user still needs to access the website during login
-# @app.after_request
-# def refresh_expiring_jwts(response):
-#     try:
-#         exp_timestamp = get_jwt()["exp"]
-#         now = datetime.now(timezone.utc)
-#         target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
-#         if target_timestamp > exp_timestamp:
-#             access_token = create_access_token(identity=get_jwt_identity())
-#             set_access_cookies(response, access_token)
-#         return response
-#     except (RuntimeError, KeyError):
-#         # Case where there is not a valid JWT. Just return the original response
-#         return response
 
 #-------------------------------------------------------------------------------------------
 # collection route (retrive all laptop info)
