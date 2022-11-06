b55242c356074c15c749ff158ef5daf40f3048c3
Muhammad Fa-aiz Bin Mohammad Fammi <2002784@sit.singaporetech.edu.sg>
2022-11-06 15:37:00 +0800

    Offline Revert

--- server/app.py
+++ server/app.py
@@ -447,7 +447,7 @@ def updateProfile(pk):
 # main driver function
 if __name__ == '__main__':
     app.config['SECRET_KEY'] = 'G$upli2St8RZxMtNeJU90'
-    app.run(host='0.0.0.0')
+    app.run(Debug=True)
 
     
     
