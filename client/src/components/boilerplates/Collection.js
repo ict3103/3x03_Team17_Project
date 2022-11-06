bc409fe2d36fc331d78421645d6da7052d6f6c2b
Muhammad Fa-aiz Bin Mohammad Fammi <2002784@sit.singaporetech.edu.sg>
2022-11-06 15:12:54 +0800

    l

--- client/src/components/boilerplates/Collection.js
+++ client/src/components/boilerplates/Collection.js
@@ -12,7 +12,7 @@ function Collection(){
     const [collectionData,setCollectionData] = useState([])
     //axios get request to get laptop collections
     useEffect(()=>{
-        axios.get("http://flask:5000/collection").then((response)=>{
+        axios.get("http://flasks:5000/collection").then((response)=>{
             setCollectionData(response.data.collection)
         })
     },[])
